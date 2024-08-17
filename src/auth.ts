ef0e2369d9269e6a31651a030300c58126dac71c
Ali <aliwebdev3986@proton.me>
2024-08-17 16:02:39 +0500

    fix auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,3 +1,4 @@
+import Mentor from '@models/Mentor';
 import User from '@models/User';
 import passport from 'passport';
 import passportJWT from 'passport-jwt';
@@ -13,14 +14,22 @@ passport.use(
     },
     async (jwtPayload, done) => {
       try {
-        const user = await User.findOne({
+        let user = await User.findOne({
           _id: jwtPayload.id,
           isActive: true,
         }).lean();
 
         if (!user) {
-          return done(null, false);
+          user = await Mentor.findOne({
+            _id: jwtPayload.id,
+            isActive: true,
+          }).lean();
+
+          if (!user) {
+            return done(null, false);
+          }
         }
+
         return done(null, user);
       } catch (err) {
         return done(err, false);
