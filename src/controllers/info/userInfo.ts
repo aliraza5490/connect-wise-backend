cb5ea366cf7d22af29fecdcbedec049b185b5880
Ali <aliwebdev3986@proton.me>
2024-08-19 23:58:23 +0500

    fix user info API

--- src/controllers/info/userInfo.ts
+++ src/controllers/info/userInfo.ts
@@ -1,3 +1,12 @@
 export default (req: IReq, res: IRes) => {
-  return res.send(req.user);
+  return res.send({
+    _id: req.user._id,
+    firstName: req.user.firstName,
+    lastName: req.user.lastName,
+    avatar: req.user?.avatar,
+    gender: req.user.gender,
+    email: req.user.email,
+    linkedInProfile: req.user.linkedInProfile,
+    bio: req.user.bio,
+  });
 };
--- src/types/mongoose.ts
+++ src/types/mongoose.ts
@@ -1,3 +1,4 @@
+import Mentor from '@models/Mentor';
 import User from '@models/User';
 
-export type UserType = InstanceType<typeof User>;
+export type UserType = InstanceType<typeof User> & InstanceType<typeof Mentor>;
