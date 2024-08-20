9bd626a19f20523d16d2f37ac8d39fd780b89b6c
Ali <aliwebdev3986@proton.me>
2024-08-20 21:31:13 +0500

    update user info API

--- src/controllers/info/userInfo.ts
+++ src/controllers/info/userInfo.ts
@@ -4,9 +4,11 @@ export default (req: IReq, res: IRes) => {
     firstName: req.user.firstName,
     lastName: req.user.lastName,
     avatar: req.user?.avatar,
+    role: req.user?.pricePerMonth ? 'mentor' : 'user',
     gender: req.user.gender,
     email: req.user.email,
     linkedInProfile: req.user.linkedInProfile,
     bio: req.user.bio,
+    isFeatured: req.user.isFeatured,
   });
 };
