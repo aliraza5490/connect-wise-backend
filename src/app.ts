b4d9765953bc9b5b5fa921b053f01b803a57470b
Ali <aliwebdev3986@proton.me>
2024-08-22 02:53:45 +0500

    register chat routes

--- src/app.ts
+++ src/app.ts
@@ -55,6 +55,7 @@ if (Number(process.env.VERCEL) === 1) {
 // --- Routes ---
 import authRoutes from '@routes/v1/auth.routes';
 import buyRoutes from '@routes/v1/buy.routes';
+import chatRoutes from '@routes/v1/chat.routes';
 import infoRoutes from '@routes/v1/info.routes';
 import mentorRoutes from '@routes/v1/mentors.routes';
 import settingsRoutes from '@routes/v1/settings.routes';
@@ -63,6 +64,7 @@ app.use('/api/v1/info', infoRoutes);
 app.use('/api/v1/buy', buyRoutes);
 app.use('/api/v1/mentor', mentorRoutes);
 app.use('/api/v1/settings', settingsRoutes);
+app.use('/api/v1/chat', chatRoutes);
 
 // error handler
 app.use((err, req, res, next) => {
