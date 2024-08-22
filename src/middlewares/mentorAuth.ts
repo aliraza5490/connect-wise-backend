4a1c0ec3b4108d443abd711e85522d6f85f715c1
Ali <aliwebdev3986@proton.me>
2024-08-22 23:38:01 +0500

    fix mentor auth middleware

--- src/middlewares/mentorAuth.ts
+++ src/middlewares/mentorAuth.ts
@@ -1,5 +1,5 @@
 export default (req: IReq, res: IRes, next: INext) => {
-  if (req.user.role !== 'mentor') {
+  if (!req.user?.pricePerMonth) {
     return res.status(403).json({
       message: 'You are not allowed to access this route',
     });
