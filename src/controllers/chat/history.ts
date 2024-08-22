e3bd86de442cc206ffec6c9b0374febdea2e3314
Ali <aliwebdev3986@proton.me>
2024-08-22 23:27:00 +0500

    fix user chat history API

--- src/controllers/chat/history.ts
+++ src/controllers/chat/history.ts
@@ -1,18 +1,15 @@
 import Chat from '@models/Chat';
 
 export default async (req: IReq, res: IRes) => {
-  let filter;
-  if (req.user.pricePerMonth) {
-    filter = {
+  if (req.user?.pricePerMonth) {
+    const chatHistory = await Chat.find({
       mentor: req.user._id,
-    };
-  } else {
-    filter = {
-      user: req.user._id,
-    };
+    }).populate('user', 'firstName lastName avatar');
+    return res.send(chatHistory);
   }
 
-  const chatHistory = await Chat.find(filter);
-
+  const chatHistory = await Chat.find({
+    user: req.user._id,
+  }).populate('mentor', 'firstName lastName avatar');
   return res.send(chatHistory);
 };
