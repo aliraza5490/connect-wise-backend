fea7c468848984a44c3adceeb5ecf4b4e3973243
Ali <aliwebdev3986@proton.me>
2024-08-23 07:47:28 +0500

    update chat history API

--- src/controllers/chat/history.ts
+++ src/controllers/chat/history.ts
@@ -1,15 +1,29 @@
 import Chat from '@models/Chat';
+import { onlineUsers } from '@root/webSockets';
 
 export default async (req: IReq, res: IRes) => {
   if (req.user?.pricePerMonth) {
     const chatHistory = await Chat.find({
       mentor: req.user._id,
     }).populate('user', 'firstName lastName avatar');
-    return res.send(chatHistory);
+    const chatHistoryWithStatus = chatHistory.map((chat) => {
+      return {
+        ...chat.toObject(),
+        status: onlineUsers.get(String(chat.user._id)) ? 'online' : 'offline',
+      };
+    });
+    return res.send(chatHistoryWithStatus);
   }
 
   const chatHistory = await Chat.find({
     user: req.user._id,
   }).populate('mentor', 'firstName lastName avatar');
-  return res.send(chatHistory);
+  const chatHistoryWithStatus = chatHistory.map((chat) => {
+    return {
+      ...chat.toObject(),
+      status: onlineUsers.get(String(chat.mentor._id)) ? 'online' : 'offline',
+    };
+  });
+
+  return res.send(chatHistoryWithStatus);
 };
