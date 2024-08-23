ca3c213577e0ed47ea8fc8c452354e08026cd832
Ali <aliwebdev3986@proton.me>
2024-08-23 22:58:02 +0500

    sort chat history

--- src/controllers/chat/history.ts
+++ src/controllers/chat/history.ts
@@ -5,7 +5,9 @@ export default async (req: IReq, res: IRes) => {
   if (req.user?.pricePerMonth) {
     const chatHistory = await Chat.find({
       mentor: req.user._id,
-    }).populate('user', 'firstName lastName avatar');
+    })
+      .populate('user', 'firstName lastName avatar')
+      .sort({ createdAt: -1 });
     const chatHistoryWithStatus = chatHistory.map((chat) => {
       return {
         ...chat.toObject(),
@@ -17,7 +19,9 @@ export default async (req: IReq, res: IRes) => {
 
   const chatHistory = await Chat.find({
     user: req.user._id,
-  }).populate('mentor', 'firstName lastName avatar');
+  })
+    .populate('mentor', 'firstName lastName avatar')
+    .sort({ createdAt: -1 });
   const chatHistoryWithStatus = chatHistory.map((chat) => {
     return {
       ...chat.toObject(),
