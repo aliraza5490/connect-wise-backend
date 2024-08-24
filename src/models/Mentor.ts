33da7dd6ac4d1b9ab9db0cb88582c6bf93f84143
Ali <aliwebdev3986@proton.me>
2024-08-24 16:50:34 +0500

    add auto index

--- src/models/Mentor.ts
+++ src/models/Mentor.ts
@@ -2,72 +2,77 @@ import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
-const MentorSchema = new Schema({
-  firstName: {
-    type: String,
-    required: [true, 'First name is required'],
+const MentorSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, 'First name is required'],
+    },
+    lastName: {
+      type: String,
+      required: [true, 'Last name is required'],
+    },
+    avatar: {
+      type: String,
+      default: '',
+    },
+    gender: {
+      type: String,
+      enum: ['Male', 'Female', 'null'],
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      match: [/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Email is not valid'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: [6, 'Password must be at least 6 characters long'],
+    },
+    linkedInProfile: {
+      type: String,
+      required: true,
+    },
+    bio: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    level: {
+      type: String,
+      required: true,
+    },
+    pricePerMonth: {
+      type: Number,
+      required: true,
+    },
+    isFeatured: {
+      type: Boolean,
+      default: false,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  lastName: {
-    type: String,
-    required: [true, 'Last name is required'],
+  {
+    autoIndex: true,
   },
-  avatar: {
-    type: String,
-    default: '',
-  },
-  gender: {
-    type: String,
-    enum: ['Male', 'Female', 'null'],
-    required: true,
-  },
-  country: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
-    match: [/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Email is not valid'],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, 'Password is required'],
-    minlength: [6, 'Password must be at least 6 characters long'],
-  },
-  linkedInProfile: {
-    type: String,
-    required: true,
-  },
-  bio: {
-    type: String,
-    required: true,
-  },
-  title: {
-    type: String,
-    required: true,
-  },
-  level: {
-    type: String,
-    required: true,
-  },
-  pricePerMonth: {
-    type: Number,
-    required: true,
-  },
-  isFeatured: {
-    type: Boolean,
-    default: false,
-  },
-  isActive: {
-    type: Boolean,
-    default: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+);
 
 MentorSchema.index({
   firstName: 'text',
